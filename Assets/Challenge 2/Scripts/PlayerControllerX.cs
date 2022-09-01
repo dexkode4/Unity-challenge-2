6c78ae1fa5f019b7a06887ff55225537ec4d119a
dexkode <dexkode4@gmail.com>
2022-09-01 19:44:32 +0100

    complete challenge

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool CanCatch = false;
+    private float spawnDogInterval = 2;
+
+      void Start()
+    {
+        InvokeRepeating("SetCatch", 0, spawnDogInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && CanCatch)
+        {
+            CatchBall();
+            CanCatch = false;
+        }
+    }
+
+    void CatchBall()
+    {
+        Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+    }
+
+    void SetCatch(){
+        CanCatch = true;
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        Debug.Log("spawnInterval here" + spawnInterval);
+    }
+
+    void Update(){
+         spawnInterval = Random.Range(3, 5);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        GameObject ballPrefab = ballPrefabs[Random.Range(0, ballPrefabs.Length)];
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefab, spawnPos, ballPrefab.transform.rotation);
+    }
+
+}
